cf62132a6a8c4e85cb6a634e28be724c01d1e080
cyz <1293165039@qq.com>
2021-09-16 15:58:34 -0500

    first commit

---
+++ AudioLabSwift/Part1ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  Part1ViewController.swift
+//  AudioLabSwift
+//
+//  Created by yinze cui on 2021/9/16.
+//  Copyright © 2021 Eric Larson. All rights reserved.
+//
+
+import UIKit
+
+class Part1ViewController: UIViewController {
+
+    @IBOutlet weak var pauseMusic: UINavigationItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //print("appear")
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(true)
+        
+        print("disappear")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ AudioLabSwift/Part3ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Part3ViewController.swift
+//  AudioLabSwift
+//
+//  Created by yinze cui on 2021/9/16.
+//  Copyright © 2021 Eric Larson. All rights reserved.
+//
+
+import UIKit
+
+class Part3ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
